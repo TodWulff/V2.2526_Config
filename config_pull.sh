1115946364c5e4c854fd8b4ad8da686d2a100a3e
Tod Wulff <tod.wulff@gmail.com>
2022-01-16 10:30:24 -0500

    printer event - automated config backup

--- config_pull.sh
+++ config_pull.sh
@@ -1,6 +1,6 @@
 cd ~/klipper_config
 git fetch
-echo "Pulling a config restore - will restart if config changes exist."
+echo "Pulling a config restore - will restart Klipper if config changes exist."
 if git status | grep -q 'Your branch is behind'; then
   git pull && echo "Changes exist, pulled & restarting Klipper" && echo 'M118 Updated configs from git, restarting...' > /tmp/printer && echo RESTART > /tmp/printer
 fi
