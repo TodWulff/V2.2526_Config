85db0e8fcd3855225f33b63d9c6b81dfa330f7d0
Tod Wulff <tod.wulff@gmail.com>
2022-01-16 10:08:19 -0500

    printer event - automated config backup

--- config_push.sh
+++ config_push.sh
@@ -1,5 +1,7 @@
+echo "Pushing a config backup."
 cd ~/klipper_config
 git fetch && git status
 git add .
-git commit -m 'printer shutdown event - automated config backup'
+git commit -m 'printer event - automated config backup'
 git push
+echo "Config backup complete."
