53d0b742ee1abd8e3501fbb7b045f7d6397ddd4f
Tod Wulff <tod.wulff@gmail.com>
2022-05-15 04:42:59 -0400

    printer event - automated config backup

---
+++ send_extruder_sms.sh
@@ -0,0 +1,1 @@
+echo "PRINTER ALERT: H2O Param Escape - Cooling and Pausing" | /home/pi/klipper_config/twilio-sms.sh +12489810005
---
+++ send_pause_sms.sh
@@ -0,0 +1,1 @@
+echo "PRINTER ALERT: Paused - Attention Needed" | /home/pi/klipper_config/twilio-sms.sh +12489810005
