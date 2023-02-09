1184f664e4bfe817564851131d122cba5e5fd80c
Tod Wulff <tod.wulff@gmail.com>
2023-02-09 16:26:54 -0500

    printer event - automated config backup

--- config_push.sh
+++ config_push.sh
@@ -1,9 +1,9 @@
 echo "Pushing a config backup."
-echo 'Pushing a config backup.' > ~/printer_data/comms/klippy.serial
+echo 'M118 Pushing a config backup.' > ~/printer_data/comms/klippy.serial
 cd ~/printer_data/config
 git fetch && git status
 git add .
 git commit -m 'printer event - automated config backup'
 git push
 echo "Config backup complete."
-echo 'Config backup complete.' > ~/printer_data/comms/klippy.serial
+echo 'M118 Config backup complete.' > ~/printer_data/comms/klippy.serial
