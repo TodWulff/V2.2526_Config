a87fa8b8fac09a852f511c7bfffaf280404b7162
Tod Wulff <tod.wulff@gmail.com>
2023-03-08 03:44:13 -0500

    printer event - automated config backup

--- log_trace_debug_event.sh
+++ log_trace_debug_event.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-#touch /home/pi/printer_data/logs/trace_debug.log &
-#echo $@ >> /home/pi/printer_data/logs/trace_debug.log &
 echo $(date +%H:%M:%S.%N) $@ >> /home/pi/printer_data/logs/trace_debug.log &
