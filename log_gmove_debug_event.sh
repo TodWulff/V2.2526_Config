3c95e20f37735f0028fea1712ff0c71059684d93
Tod Wulff <tod.wulff@gmail.com>
2023-03-09 00:19:21 -0500

    printer event - automated config backup

--- log_gmove_debug_event.sh
+++ log_gmove_debug_event.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-#touch /home/pi/printer_data/logs/gmove_debug.log &
-#echo $@ >> /home/pi/printer_data/logs/gmove_debug.log &
 echo $(date +%H:%M:%S.%N) $@ >> /home/pi/printer_data/logs/gmove_debug.log &
--- log_svv_debug_event.sh
+++ log_svv_debug_event.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-#touch /home/pi/printer_data/logs/svv_debug.log &
-#echo $@ >> /home/pi/printer_data/logs/svv_debug.log &
 echo $(date +%H:%M:%S.%N) $@ >> /home/pi/printer_data/logs/svv_debug.log &
