740c16315dfae5bb9bd438cec5421187ebb4443b
Tod Wulff <MegaHurtz@engineer.com>
2023-03-16 07:44:32 -0400

    reverted unintentional reversion of proc entry/exit trace debug code

--- config_pull.sh
+++ config_pull.sh
@@ -8,6 +8,6 @@ if git status | grep -q 'Your branch is behind'; then
 	echo 'RESTART' > ~/printer_data/comms/klippy.serial
 else
 	say_wait No changes
-	echo 'UPDATE_DELAYED_GCODE ID=_advise_printer_state DURATION=0.1' > ~/printer_data/comms/klippy.serial
+	# echo 'UPDATE_DELAYED_GCODE ID=_advise_printer_state DURATION=0.1' > ~/printer_data/comms/klippy.serial
 
 fi
