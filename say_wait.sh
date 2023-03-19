722d27f974e5d377ffd24749631224bdf0285b6b
Tod Wulff <MegaHurtz@engineer.com>
2023-03-19 03:58:53 -0400

    Polished the UI aspects wrt speech enabled state

--- say_wait.sh
+++ say_wait.sh
@@ -10,12 +10,8 @@ do
 	phrase="$phrase $element"
 done
 
-#writes to klipper console
-echo "M118 <font color=gray>Brody🔊: </font><font color=lime>$phrase</font>" > ~/printer_data/comms/klippy.serial
-
 #generate tts audio file
 gtts-cli "$phrase" -o /home/pi/temp/temp.wav > ~/temp/temp_gtts.log 2>&1
-#pico2wave -l en-US -w ~/temp/temp.wav "$phrase" > /dev/null 2>&1 &
 
 #play the tts audio, returning immediately
 cvlc --gain 1.5 --quiet --rate 1.25 --play-and-exit ~/temp/temp.wav > ~/temp/temp_cvlc.log 2>&1 
